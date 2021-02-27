b7b2fa5ddf946a3857269d0170a2b63db404c316
Matthew_Sharp <matthew.sharp1@ontariotechu.net>
2021-02-27 14:53:13 -0500

    Added Directory Selector

---
+++ Assignment_Code/src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File("."));
+        File mainDirectory = directoryChooser.showDialog(primaryStage);
+
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
